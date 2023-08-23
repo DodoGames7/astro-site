79ee37e5aa4a7fb1c140dfafa0638f0f9af1e72a
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2023-08-23 19:19:25 +0300

    test

---
+++ astro.config.mjs
@@ -0,0 +1,32 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+               site: "https://DodoGames7.github.io",
+               base: "/astro-site",
+	integrations: [
+		starlight({
+			title: 'dodoGames stuff',
+			social: {
+				github: 'https://github.com/withastro/starlight',
+			},
+			sidebar: [
+				{
+					label: 'Stuff',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Projects', link: '/general/projects/' },
+					],
+				},
+				{
+					label: 'Development',
+					autogenerate: { directory: 'development' },
+				},
+			],
+		}),
+	],
+
+	// Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
+	image: { service: { entrypoint: 'astro/assets/services/sharp' } },
+});
