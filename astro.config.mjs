51498ac0c1a52bd95441dbde6207fdeff962a0c3
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2023-08-26 00:18:38 +0300

    changed introduction url

--- astro.config.mjs
+++ astro.config.mjs
@@ -23,7 +23,7 @@ export default defineConfig({
 				{
 					label: "Home",
 					items: [
-						{ label: 'Introduction', link: '/home/greeting'}
+						{ label: 'Introduction', link: '/home/introduction'}
 					],
 				},
 				{
